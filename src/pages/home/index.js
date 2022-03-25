e8ea2035f4b85c47a061a79b0104dbefa7003e7c
isnanibarokah <isnanibarokah@gmail.com>
2022-03-25 12:57:35 +0700

    Modify import file

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,17 +1,12 @@
+import Searchbar from "../../components/searchbar";
+import Gif from "../../components/gif";
+import Data from "../../components/gif/data";
+
 function Mario() {
     return (
       <div className="container">
-        <form id="form" className="formSearch">
-          <div className="form-group">
-            <input type="text" name="title"/>
-            <button id="btnSearch" type="submit">Search</button>
-          </div>
-        </form>
-        <img
-          src="https://media.giphy.com/media/Vh8pbGX3SGRwFDh3V0/source.gif"
-          className="image"
-          alt="GIF"
-        />
+        <Searchbar />
+        <Gif title={Data.title} url={Data.url} />
       </div>
     );
   }
